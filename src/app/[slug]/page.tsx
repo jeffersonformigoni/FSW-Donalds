2c303c882a5c9e5c67965342098867c6e44015b1
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-02-23 17:26:34 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import * as navigation from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return navigation.notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* BEM VINDO */}
+      <div className="space-y-2 pt-24 text-center">
+        <div className="text-2xl font-semibold">Seja bem-vindo!</div>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/dine_in.png"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/takeaway.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
+
+// server components - renderizados no servidor
+// client components - renderizados no cliente
+// pode ser async
+// podem chamar recursos do back-end (banco de dados)
+// não pode usar hooks
+// não pode usar useEffect
+// não pode usar useState
+// não pode usar context
+// não pode usar eventos - interatividade
+// tambem não conseguimos acessar recursos do servidor, copmo chamar o banco de dados, por exemplo, dentro de Client Components.
